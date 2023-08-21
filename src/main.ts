2f005d2fc033510a449d8fc78e4834b3a7ddc2cd
Rene Navarrete <renavarretemx@gmail.com>
2023-08-21 14:57:27 -0600

    Add
    Initial configuration with Bootstrap.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 
 createApp(App).mount('#app')
