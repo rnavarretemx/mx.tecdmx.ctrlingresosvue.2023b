3465e841c3bf075801b36989a1f7ec5baf7f417e
Rene Navarrete <renavarretemx@gmail.com>
2023-08-22 19:08:16 -0600

    Add
    Config forms with JSON component.

---
+++ src/components/input_text.vue
@@ -0,0 +1,18 @@
+<template sm="4" md="6" lg="4">
+    <div class="input_text">
+        <label :for="`type-${block}`"> {{ block.label }} </label>
+        <b-form-input placeholder="input_text"></b-form-input>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        block: Object
+    },
+    name: "input_text"
+};
+
+</script>
+
+<style scoped></style>
--- src/components/views/FormRegistro.vue
+++ src/components/views/FormRegistro.vue
@@ -1,38 +1,94 @@
 <template>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-        <b-container>
-            <b-row class="">
+    <b-form>
+        <b-row  >
+            <b-col>
+                <template  lg="4"  v-for="block in content.body">
+                    <component :is="block.component" :block="block"></component>
+                </template>
 
-                <!-- <b-col sm="12" md="6" lg="4" v-for="type in types" :key="type">
-
-                    <label :for="`type-${type}`"> {{ type.label }} </label>
-                    
-                    <b-form-input v-if="!type.options" :id="`type-${type}`" :type="type.type"></b-form-input>
-                    <b-form-select v-if="type.options" :options="type.options"></b-form-select>
-                </b-col> -->
-
-            </b-row>
-        </b-container>
-
+            </b-col>
+        </b-row>
     </b-form>
 </template>
+
+
+<script>
+import Bar from "../Bar";
+import Foo from "../Foo";
+import input_text from "../input_text"
+
+export default {
+    data() {
+        return {
+            content: {
+                body: [
+                    { component: input_text, type: 'text', label: 'Nombre' },
+                    { component: input_text, type: 'text', label: 'Apellido Paterno' },
+                    { component: input_text, type: 'text', label: 'Apellido Materno' },
+                    { component: input_text, type: 'text', label: 'Procedencia' },
+                    { component: input_text, type: 'text', label: 'Asunto' },
+                    { component: input_text, type: 'tel', label: 'Contacto' },
+                    { component: input_text, type: 'select', label: '¿A quién visita?', options: ['Carrots', 'Beans', 'Tomatoes', 'Corn'] },
+                    { component: input_text, type: 'text', label: 'Área o visita?' },
+                    { component: input_text, type: 'number', label: 'Extensión' },
+                    { component: input_text, type: 'datepicker', label: 'Fechas disponibles' },
+                    { component: input_text, type: 'timepicker', label: 'Hora' }
+                ]
+            }
+        };
+    },
+    name: "App",
+    components: {
+        Bar,
+        Foo
+    }
+};
+</script>
     
-<script lang="ts" setup>
+<!-- <script lang="ts" setup>
+
+import input_text from '../input_text.vue';
+import Foo from "../Foo.vue";
+import Bar from "../Bar.vue";
+
 var nombre, paterno, materno, fecha, hora, org_inst, asunto, contacto, personal, area, extension;
 var show = true;
 
+const components:any = {
+    Bar,
+    Foo
+  }
+
+const  body:any = [
+        {
+            uid: "BUY6Drn9e1",
+            component: "foo",
+            headline: "Foo"
+        },
+        {
+            _uid: "gJZoSLkfZV",
+            component: "bar",
+            title: "Bar"
+        },
+        {
+            _uid: "X1JAfdsZxy",
+            component: "foo",
+            headline: "Another headline"
+        }
+    ]
+
 const types: any = [
-    { type: 'text', label: 'Nombre' },
-    { type: 'text', label: 'Apellido Paterno' },
-    { type: 'text', label: 'Apellido Materno' },
-    { type: 'text', label: 'Procedencia' },
-    { type: 'text', label: 'Asunto' },
-    { type: 'tel', label: 'Contacto' },
-    { type: 'select', label: '¿A quién visita?', options: ['Carrots', 'Beans', 'Tomatoes', 'Corn'] },
-    { type: 'text', label: 'Área o visita?' },
-    { type: 'number', label: 'Extensión' },
-    { type: 'datepicker', label: 'Fechas disponibles' },
-    { type: 'timepicker', label: 'Hora' }
+    { component: input_text, type: 'text', label: 'Nombre' },
+    { component: input_text, type: 'text', label: 'Apellido Paterno' },
+    { component: input_text, type: 'text', label: 'Apellido Materno' },
+    { component: input_text, type: 'text', label: 'Procedencia' },
+    { component: input_text, type: 'text', label: 'Asunto' },
+    { component: input_text, type: 'tel', label: 'Contacto' },
+    { component: input_text, type: 'select', label: '¿A quién visita?', options: ['Carrots', 'Beans', 'Tomatoes', 'Corn'] },
+    { component: input_text, type: 'text', label: 'Área o visita?' },
+    { component: input_text, type: 'number', label: 'Extensión' },
+    { component: input_text, type: 'datepicker', label: 'Fechas disponibles' },
+    { component: input_text, type: 'timepicker', label: 'Hora' }
 ]
 
 
@@ -50,6 +106,8 @@ form_registro_cita: [
     extension = '',
 ]
 
+
+
 foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn']
 
 const onSubmit = (e): void => {
@@ -58,15 +116,15 @@ const onSubmit = (e): void => {
 const onReset = (e): void => {
     nombre = ''
 }
-</script>
+</script> -->
     
 <style scoped>
 .row {
     border: solid 1px red;
 }
 
-.col{
-    border: solid 1px blue;
+.col {
+    border: solid 5px blue;
 }
 
 .titulo_uno {
